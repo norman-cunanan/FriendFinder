f921ce41eb1b1bc7f6f56493ab386b14ca21e51b
Norman Cunanan <norman.cunanan17@gmail.com>
2017-08-31 07:49:55 -0700

    initial files

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//Routes apiDATA first where we pulling data to display in HTML
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+//Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+}); 
